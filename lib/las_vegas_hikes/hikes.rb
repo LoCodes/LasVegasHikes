4c8d6d5e13641ad7c7698afc62e961fed58c89aa
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-16 12:03:10 +0000

    Find out how to scrape text with no selectors/types!

--- lib/las_vegas_hikes/hikes.rb
+++ lib/las_vegas_hikes/hikes.rb
@@ -16,13 +16,68 @@ class LasVegasHikes::Hikes
   def self.scrape_explore1
     doc = Nokogiri::HTML(open("https://www.explore-mag.com/The-Five-Best-Hikes-Near-Las-Vegas"))
     title = doc.search("h1.alignLeft").text  # move title elsewhere later?
-    
+    name = doc.search("h2")[1].text
+    location = doc.search("h3")[0].text
+    length = "2.4 km roundtrip, out-and-back trail"    doc.search("strong")[0].text
+    time = "1 hour"
+    difficulty = doc.search("strong")[2].text
+    elevation = doc.search("strong")[3].text
+    readmore =
+
     binding.pry
   end
 end
 
 
 
+   second hike
+
+    name = doc.search("h2")[2].text
+    location = doc.search("h3")[1].text
+    length =
+    time =
+    difficulty =
+    elevation =
+    readmore =
+
+
+
+
+
+     third hike
+
+    name = doc.search("h2")[3].text
+    location = doc.search("h3")[2].text
+    length =
+    time =
+    difficulty =
+    elevation =
+    readmore =
+
+
+
+
+     4th hikes
+
+     name = doc.search("h2")[4].text
+     location = doc.search("h3")[3].text
+     length =
+     time =
+     difficulty =
+     elevation =
+     readmore =
+
+
+
+    5th hike
+
+    name = doc.search("h2")[5].text
+    location = doc.search("h3")[4].text
+    length =
+    time =
+    difficulty =
+    elevation =
+    readmore =
 
   # go to article, find hike
   # extract the properties

d2c896771ae27f348059ba450969b18589bf5846
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-16 22:55:07 +0000

    FIX THIS

--- lib/las_vegas_hikes.rb
+++ lib/las_vegas_hikes.rb
@@ -5,6 +5,7 @@ require 'pry'
 require_relative "./las_vegas_hikes/version"
 require_relative './las_vegas_hikes/cli'
 require_relative './las_vegas_hikes/hikes'
+require_relative './las_vegas_hikes/scraper.rb'
 
 
 
